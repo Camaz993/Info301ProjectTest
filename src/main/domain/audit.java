81ef35f39bc064e7cb47a416ea445b4d230138f9
Laurie Lloyd-Jones <laurielloydjones@gmail.com>
2019-08-11 14:23:54 +1200

    added plugins

--- src/main/domain/audit.java
+++ src/main/domain/audit.java
@@ -1,6 +1,19 @@
 package main.domain;
+
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 public class audit {
 	private Integer auditid;
