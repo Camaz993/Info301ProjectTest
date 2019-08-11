f0c9c02d9cbac4a18711a7471752d654c7f74b1d
nataliegrattan <nataliecgrattan@gmail.com>
2019-08-12 00:04:02 +1200

    Relationships implemented for in_negotiation, operative, status and user

--- src/main/domain/operative.java
+++ src/main/domain/operative.java
@@ -1,22 +1,75 @@
 package main.domain;
 import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
+@Entity
+@Table(name = operative.TABLE_NAME)
+//@SecondaryTable (name = Contract.TABLE_NAME)
 public class operative {
-	//private Integer requestid;
+	
+	@Id
+	@Column(name = "requestid")
+	private Integer requestid;
+	
+	@NotNull
+	@Column(name = "date_signed")
 	private Date date_signed;
+	
+	@NotNull
+	@Column(name = "date_commenced")
 	private Date date_commenced;
+	
+	@NotNull
+	@Column(name = "date_expire")
 	private Date date_expire;
+	
+	@NotNull
+	@Column(name = "verification")
 	private String verification;
 	
-	public operative(Integer requestid, Date date_signed, Date date_commenced, Date date_expire, String verification) {
+	@OneToOne
+    @MapsId
+	private Contract contract;
+	
+	//Constructor method
+	public operative(Integer requestid, Date date_signed, Date date_commenced, Date date_expire, String verification, Contract contract) {
 		super();
-		//this.requestid = requestid;
+		this.requestid = requestid;
 		this.date_signed = date_signed;
 		this.date_commenced = date_commenced;
 		this.date_expire = date_expire;
 		this.verification = verification;
+		this.contract = contract;
 	}
 
+	//Getters and setters
+	public Integer getRequestId() {
+		return requestid;
+	}
+
+	public void setRequestId(Integer requestid) {
+		this.requestid = requestid;
+	}
+	
+	public Contract getContract() {
+		return contract;
+	}
+	
+	public void setContract() {
+		this.contract = contract;
+	}
+	
 	public Date getDate_signed() {
 		return date_signed;
 	}
--- src/main/domain/status.java
+++ src/main/domain/status.java
@@ -1,17 +1,46 @@
 package main.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = Status.TABLE_NAME)
+//@SecondaryTable (name = Contract.TABLE_NAME)
 public class status {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "statusid")
+	@OnetoOne(mappedBy = previousstatus)
 	private Integer statusid;
-	private Integer previousstatus;
+	
+	//Foreign key of statusid (self-referencing integrity)
+	@OneToOne
+	private Status previousstatus;
+	
+	@NotNull
+	@Column(name = "active")
 	private String active;
 	
-	public status(Integer statusid, Integer previousstatus, String active) {
+	//Constructor
+	public status(Integer statusid, Status previousstatus, String active) {
 		super();
 		this.statusid = statusid;
 		this.previousstatus = previousstatus;
 		this.active = active;
 	}
 
+	//Getters and setters
 	public Integer getStatusid() {
 		return statusid;
 	}
@@ -20,11 +49,11 @@ public class status {
 		this.statusid = statusid;
 	}
 
-	public Integer getPreviousstatus() {
+	public Status getPreviousstatus() {
 		return previousstatus;
 	}
 
-	public void setPreviousstatus(Integer previousstatus) {
+	public void setPreviousstatus(Status previousstatus) {
 		this.previousstatus = previousstatus;
 	}
 
