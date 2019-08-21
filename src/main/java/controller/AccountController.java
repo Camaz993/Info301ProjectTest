900e53ceed9a4b8740c8df5400ab44956689bbe2
aveal466 <aveal466@student.otago.ac.nz>
2019-08-21 13:47:29 +1200

    Made account controller to begin adding accounts

---
+++ src/main/java/controller/AccountController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import domain.contract;
+import domain.status;
+import domain.user;
+import service.IContractService;
+
+@RestController
+public class AccountController {
+	
+	@PostMapping("/api/staff")
+
+}
