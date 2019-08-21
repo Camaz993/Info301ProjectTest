1754bc9b5762f9a4cba72fecf05e405d5e65b7e7
aveal466 <aveal466@student.otago.ac.nz>
2019-08-21 13:55:57 +1200

    Added functionality to Account Controller and added services and repository

--- src/main/java/controller/AccountController.java
+++ src/main/java/controller/AccountController.java
@@ -10,14 +10,35 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import domain.contract;
-import domain.status;
 import domain.user;
-import service.IContractService;
+import service.IAccountService;
 
 @RestController
 public class AccountController {
 	
 	@PostMapping("/api/staff")
+	public ResponseEntity<String> createAccount(@RequestParam String first_name, @RequestParam String last_name, @RequestParam String role, 
+			@RequestParam String username, @RequestParam String password, @RequestParam boolean locked)
+	
+	{
+		Account account = new Account();
+		account.setFirstName(first_name);
+		account.setLastName(last_name);
+		account.setRole(role);
+		account.setUsername(username);
+		account.setPassword(password);
+		account.setLocked(locked);
+		
+		return new ResponseEntity<String>(HttpStatus.CREATED);
+		
+	}
+	
+	
+	this.first_name = first_name;
+	this.last_name = last_name;
+	this.role = role;
+	this.username = username;
+	this.password = password;
+	this.locked = locked;
 
 }
