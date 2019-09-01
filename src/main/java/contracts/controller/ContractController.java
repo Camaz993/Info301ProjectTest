1c2c1954af28d04e82e92e4d1e6fb57b2311c477
nataliegrattan <nataliecgrattan@gmail.com>
2019-09-02 01:29:58 +1200

    Updated search

--- src/main/java/contracts/controller/ContractController.java
+++ src/main/java/contracts/controller/ContractController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -44,8 +45,9 @@ public class ContractController {
 	
 	
 	@GetMapping("/search_contracts")
-	public List<contract> getAllContracts() {
-		return contractService.getAllContracts();
+	public String getAllContracts(Model model) {
+		model.addAttribute("contracts", contractService.getAllContracts());
+		return "search_contracts";
 	}
 	
 	@PostMapping("/api/contracts/search")
