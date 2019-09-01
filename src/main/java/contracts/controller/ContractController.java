d62d7ccd029b2e5da0e9743ee68e5b5af09fdc24
nataliegrattan <nataliecgrattan@gmail.com>
2019-09-02 00:35:59 +1200

    Began to implement search

--- src/main/java/contracts/controller/ContractController.java
+++ src/main/java/contracts/controller/ContractController.java
@@ -5,17 +5,18 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+
 
 import contracts.domain.contract;
 import contracts.domain.status;
 import contracts.domain.user;
 import contracts.service.IContractService;
 
-@RestController
+@Controller
 public class ContractController {
 	
 	@Autowired
