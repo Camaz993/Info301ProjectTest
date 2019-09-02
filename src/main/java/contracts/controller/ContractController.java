2ad6828ce55bbc35312419541b415e9896e8c62e
nataliegrattan <nataliecgrattan@gmail.com>
2019-09-02 13:04:54 +1200

    Combining pages and css

--- src/main/java/contracts/controller/ContractController.java
+++ src/main/java/contracts/controller/ContractController.java
@@ -1,8 +1,10 @@
 package contracts.controller;
 
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -46,7 +48,9 @@ public class ContractController {
 		
 		contractService.addContract(contract);
 		
-		return new ResponseEntity<String>(HttpStatus.CREATED);
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Location", "/");
+		return new ResponseEntity<String>(headers, HttpStatus.MOVED_PERMANENTLY);
 	}
 	
 	@GetMapping("/search_contracts")
