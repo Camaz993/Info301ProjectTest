cf820d5fd067208cb724421b63896b1e2776cb1b
aveal466 <aveal466@student.otago.ac.nz>
2019-10-08 17:07:36 +1300

    Icon shows up on HTML homepage and in line with title

--- src/main/java/contracts/controller/ContractController.java
+++ src/main/java/contracts/controller/ContractController.java
@@ -1,5 +1,6 @@
 package contracts.controller;
 
+import java.io.File;
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -18,9 +19,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.google.common.io.Files;
 
 import contracts.domain.Audit;
 import contracts.domain.Contract;
@@ -482,5 +486,6 @@ public class ContractController {
 		auditService.addAudit(blank);
 		return "redirect:/view_details/" + contract.getRequestid();
 	}
+
 }
 	
